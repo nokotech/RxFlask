49b916b1970d88992f09d972f70aae957a3985b4
たけのこ <michaelbobu@gmail.com>
2018-06-28 22:51:57 +0900

    fix docker.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # RxFlask
 
+##
+* `sudo pip install -r ./.config/requirements.txt`
+
 ## none .pyc
 * Windows: `set PYTHONDONTWRITEBYTECODE=1`
 * Mac `export PYTHONDONTWRITEBYTECODE=1`
---
+++ src/model/repository/MongoManager.py
@@ -0,0 +1,29 @@
+from threading import Lock
+from pymongo import MongoClient
+
+class MongoManager(object):
+    _instance = None
+    _lock = Lock()
+    _db = None
+
+    def __init__(self):
+        print('init')
+    
+    def __new__(cls):
+        print('pre new')
+        with cls._lock:
+            if cls._instance is None:
+                cls._preInit(cls)
+        return cls._instance
+    
+    def _preInit(self):
+        print('pre init')
+        self._instance = super().__new__(self)
+        connect = MongoClient('0.0.0.0', 32768)
+        self._db = connect.test
+        print('db name is = {}'.format(self._db.name))
+    
+    @property
+    def db(self):
+        print("getDB {}".format(self._db))
+        return self._db
